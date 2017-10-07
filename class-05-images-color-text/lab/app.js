319cb2c60d911d76ab87c3fbda24af379b4c4aff
dknore <darcy.knore@gmail.com>
2017-10-07 14:06:39 -0700

    question1 tested & working

--- class-05-images-color-text/lab/app.js
+++ class-05-images-color-text/lab/app.js
@@ -18,7 +18,7 @@ function sum(a,b){ //eslint-disable-line
 }
 
 // TODO: Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -31,11 +31,13 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // TODO: Write your code here
-function multiply(a,b){ //eslint-disable-line
-
-}
-
-// TODO: Here is the test for multiply(); uncomment it to run it
+// function multiply(a,b){ //eslint-disable-line
+//   var theProduct = a * b;
+//   var message = 'The product of ' + a + ' and ' + b + ' is ' + theProduct + '.';
+//   return [theProduct, message];
+// }
+//
+// // TODO: Here is the test for multiply(); uncomment it to run it
 // testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
